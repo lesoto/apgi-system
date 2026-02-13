562105867cc08752a159916de149c4f1f1d90a89
lesoto <lesoto@lesoto.com>
2026-02-13 13:28:42 -0500

    removed standalone API app

--- standalone-api/app/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Standalone APGI API Application
-
-This package contains the standalone REST API for the APGI system.
-"""
-
-__version__ = "1.0.0"
--- standalone-api/app/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility modules for the standalone API."""
-
-from .dependency_checker import check_dependencies
-
-__all__ = ["check_dependencies"]
